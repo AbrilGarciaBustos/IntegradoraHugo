ceddb325d15710e627b4a62f879d805ba72eb7bd
Abril Garcia Bustos <20223tn053@utez.edu.mx>
2024-04-15 21:00:01 -0600

    Ya listo el servicio

--- IntegradoraHugo/src/routes/authRoutes.js
+++ IntegradoraHugo/src/routes/authRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { ObjectId } = require("mongodb"); 
+const jwt = require("jsonwebtoken"); // Importa el módulo jsonwebtoken
+
+// Define una clave secreta para firmar los tokens JWT
+const secretKey = "tu_clave_secreta"; 
 
 //Metodod de registro 
 router.post("/register", async (req, res) => {
@@ -27,22 +31,24 @@ router.post("/register", async (req, res) => {
   }
 });
 
-module.exports = router;
-
-//Metodo de inicio de sesión
+// Metodo de inicio de sesión
 router.post("/signin", async (req, res) => {
   try {
     const database = req.db;
     const credentials = req.body;
     const persona = database.collection('persona');
 
-    // Comprobar si existen en la base de datos
+    // Comprobar si las credenciales son válidas en la base de datos
     const user = await persona.findOne({ nombre_usuario: credentials.nombre_usuario, contrasenia: credentials.contrasenia });
     if (!user) {
       return res.status(404).send({ message: "Credenciales inválidas" });
     }
 
-    res.status(200).send({ message: "Inicio de sesión exitoso" });
+    // Si las credenciales son válidas, genera un token JWT
+    const token = jwt.sign({ id_usuario: user.id_usuario }, secretKey, { expiresIn: '1h' }); // El token expira en 1 hora
+
+    // Envía el token al cliente como respuesta
+    res.status(200).send({ message: "Inicio de sesión exitoso", token });
   } catch (error) {
     console.error("Error al iniciar sesión:", error);
     res.status(500).send({ message: "Error interno del servidor" });
--- IntegradoraHugo/src/services/app.js
+++ IntegradoraHugo/src/services/app.js
@@ -1,38 +1,63 @@
-const express = require('express');
-const cors = require('cors'); // Importa el middleware cors
+const express = require("express");
+const cors = require("cors"); // Importa el middleware cors
+const jwt = require("jsonwebtoken"); // Importa el módulo jsonwebtoken
+const authRoutes = require("../routes/authRoutes");
+const { connect } = require("../config/database");
+const fileRouthes = require("../routes/fileRouthes");
+require("../config/database");
+
 const app = express();
-const authRoutes = require('../routes/authRoutes');
-const { connect } = require('../config/database')
-const fileRouthes = require('../routes/fileRouthes');
-require('../config/database');
 
 app.use(express.json());
 
-app.use(cors({
-    origin: 'http://127.0.0.1:5500',
-    methods: ['GET', 'POST','PUT','DELETE'] // Permitir métodos GET y POST desde el origen especificado
-}));
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST", "PUT", "DELETE"], // Permitir métodos GET y POST desde el origen especificado
+  })
+);
 
+// Middleware para verificar y decodificar el token JWT
+function verificarToken(req, res, next) {
+  // Obtener el token del encabezado de autorización
+  const token = req.headers.authorization;
+
+  // Verificar si el token existe
+  if (!token) {
+    return res.status(401).json({ mensaje: "Token no proporcionado" });
+  }
+
+  // Verificar y decodificar el token
+  jwt.verify(token.split(" ")[1], "tu_clave_secreta", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ mensaje: "Token inválido" });
+    }
+
+    // Almacenar la información del usuario decodificada en el objeto de solicitud
+    req.usuario = decoded;
+    next();
+  });
+}
 
 app.use(async (req, res, next) => {
-    try {
-        req.db = await connect(); // Conecta a la base de datos
-        next(); // Continúa con la siguiente función de middleware o ruta
-    } catch (error) {
-        console.error('Error al conectar con la base de datos:', error);
-        res.status(500).send('Error interno del servidor'); // Envía una respuesta de error al cliente
-    }
+  try {
+    req.db = await connect(); // Conecta a la base de datos
+    next(); // Continúa con la siguiente función de middleware o ruta
+  } catch (error) {
+    console.error("Error al conectar con la base de datos:", error);
+    res.status(500).send("Error interno del servidor"); // Envía una respuesta de error al cliente
+  }
 });
 
-app.use('/api/auth', authRoutes);
-app.use("/api/file", fileRouthes);
+app.use("/api/auth", authRoutes);
+app.use("/api/file", verificarToken, fileRouthes); // Aplicar el middleware de verificación del token JWT a las rutas protegidas
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Servidor corriendo');
+app.get("/", (req, res) => {
+  res.send("Servidor corriendo");
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
